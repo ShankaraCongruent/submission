41c1eaaa7ff69571e45d0be13c86708134628f73
Shankaranarayanan V <shankaranarayanan.v@cspl.com>
2025-11-04 22:37:55 +0530

    cw on nov_04

---
+++ cw/nov_04index.js
@@ -0,0 +1,46 @@
+import {
+    basicMath,
+    closureExample,
+    objectExample,
+    symbolExample,
+    symbolPrimitiveExample,
+    forOfLoop,
+    classExample,
+    iteratorExample,
+    generatorExample,
+    sumExample,
+    logArrayElements,
+    entriesExample,
+    mapJsonExample,
+    flattenExample,
+    destructuringExample,
+    defaultValuesExample,
+    reverseExample,
+    squaresExample,
+    runAllExamples
+  } from "./nov_04.js";
+  
+  // Call each individually
+  console.log("=== Running Individual Examples ===");
+  basicMath();
+  closureExample();
+  objectExample();
+  symbolExample();
+  symbolPrimitiveExample();
+  forOfLoop();
+  classExample();
+  iteratorExample();
+  generatorExample();
+  sumExample();
+  logArrayElements();
+  entriesExample();
+  mapJsonExample();
+  flattenExample();
+  destructuringExample();
+  defaultValuesExample();
+  reverseExample();
+  squaresExample();
+  
+  console.log("\n=== Running All Examples Together ===");
+  runAllExamples();
+  
